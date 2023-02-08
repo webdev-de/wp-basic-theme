6dd3bfc740bc05afc22d9d1092e30dfdb97536ea
Jan Behrens <mister.webdev.de@gmail.com>
2023-02-08 21:14:59 +0100

    update responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 const mwd_info = 'Javascript wurde erfolgreich geladen';
 console.info(mwd_info);
+
+const menuButton = document.querySelector('.menu-button');
+const menuContainer = document.querySelector('.menu-container ul');
+
+menuButton.addEventListener('click', () => {
+  menuContainer.classList.toggle('menu-open');
+});
